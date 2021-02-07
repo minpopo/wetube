0eaed449a31797bbf466f74c0671b63118a186e7
김민정 <minpopo1024@gmail.com>
2021-02-07 19:55:56 +0900

    First EXPRESS Server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hi");
+// 서버 종료: ctrl+c,  서버실행: npm start
+
+const express = require('express'); //express 파일을 가져오기
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
+  
